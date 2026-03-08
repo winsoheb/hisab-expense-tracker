cb6c00d4318cf6c4c5bfc2b4b4e046e84a5dc589
Soheb <soheb@hisab.in>
2026-03-08 12:43:20 +0530

    Fix: add Netlify app to allowed CORS origins

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://hisab.soheb.in', 'http://localhost:5000', 'http://127.0.0.1:5000'],
+    origin: ['https://hisab.soheb.in', 'https://hisabsoheb.netlify.app', 'http://localhost:5000', 'http://127.0.0.1:5000', 'http://localhost:8080'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
